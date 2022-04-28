6279dd3079f2bee8eb0fb4c41ddf4610abf95a2a
Martin Lynn <54124417+martin473@users.noreply.github.com>
2022-04-27 21:10:30 -0400

    Delete hospital directory

--- hospital/php/dbhmsconfig.php
+++
@@ -1,4 +0,0 @@
-<?php
-$config = parse_ini_file('login.ini');
-$conn = mysqli_connect($config['dbhost'], $config['username'], $config['password'], $config['db']);
-mysqli_select_db($conn, $config['db']);
